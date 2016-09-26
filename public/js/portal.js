1d4c465ce4c22500dc32dfca1bde501d2eb496ad
Rodrigo Marques da Silva <15153827@suicobrasileira.sp>
2016-09-26 15:46:14 -0300

    alteracoes e exibicao de dados nas paginas v1.0

--- public/js/portal.js
+++ public/js/portal.js
@@ -1,11 +1,26 @@
+
 $(document).ready(function() {
-	$('#content-portal table').DataTable({
-		"language": {
-			"url": "https://cdn.datatables.net/plug-ins/1.10.12/i18n/Portuguese-Brasil.json"
+	setTimeout(function() {
+		$('#content-portal table').DataTable({
+			"language": {
+				"url": "https://cdn.datatables.net/plug-ins/1.10.12/i18n/Portuguese-Brasil.json"
+			},
+			"dom": '<"top"<"col-xs-6"i>f>tr<"bottom"<"col-xs-6"l><"col-xs-6"p>>',
+			"order": [[ 0, "desc" ]]        
+		});
+	},200);
+	$.ajax({
+		url: "/dados/advertencia",
+		success: function(response) {
+			for (var i = 0; i <= response.length - 1; i++) {
+				$( "#content-portal table tbody").append( "<tr><td>"+response[i].data+"</td><td>Setor "+response[i].id_setor+"</td><td>"+response[i].titulo+"</td><td>"+response[i].id_usuario+"</td><td>"+response[i].mensagem+"</td></tr>" );
+			};
 		},
-		"dom": '<"top"<"col-xs-6"i>f>tr<"bottom"<"col-xs-6"l><"col-xs-6"p>>',
-		"order": [[ 0, "desc" ]]        
-	});
+		error: function(xhr) {
+		//Do Something to handle error
+	}
+});
+	
 });
 
 $.ajax({
