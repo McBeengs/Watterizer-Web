2f31ea2b2ed0ff371a8e0e465c672f95e4a8d825
Kinsley Ajie Pereira <15153821@suicobrasileira.sp>
2016-09-26 14:15:48 -0300

    Redirecionamento do portal corrigido

--- public/js/portal.js
+++ public/js/portal.js
@@ -1,7 +1,4 @@
 $(document).ready(function() {
-	$.fn.dataTable.moment( 'HH:mm MMM D, YY' );
-    $.fn.dataTable.moment( 'dddd, MMMM Do, YYYY' );
-
 	$('#content-portal table').DataTable({
 		"language": {
 			"url": "https://cdn.datatables.net/plug-ins/1.10.12/i18n/Portuguese-Brasil.json"
