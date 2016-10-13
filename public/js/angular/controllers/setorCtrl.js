aa33206a1b6366cec95a3e045cb9c27e18cdd597
Kinsley Ajie Pereira <15153821@suicobrasileira.sp>
2016-10-13 16:25:48 -0300

    Setores com seus respectivos arduinos

--- public/js/angular/controllers/setorCtrl.js
+++ public/js/angular/controllers/setorCtrl.js
@@ -1,24 +1,11 @@
 // CONTROLLER DE SETORES
 app.controller('setorCtrl', function($scope, $http) {
 	// LISTA TODOS OS SETORES
-	$http.get("/setor")
+	$http.get("/setor/arduino")
 	.then(function (response) {
 		$scope.setores = response.data;
-		$scope.setores.arduinos = new Array();
+		console.log($scope.setores);
 	}, function(response){
 		console.log("Falhou")
 	});
-
-	$http.get("/dados/arduino")
-	.then(function (response) {
-		for (var i = response.length - 1; i >= 0; i--) {
-			for (var i = $scope.setores.length - 1; i >= 0; i--) {
-				if(response[i].id_setor = $scope.setores[i].id){
-					$scope.setores.arduinos.push(response[i]);
-					break;
-				}
-			}
-		}
-	});
-	console.log($scope.setores)
 });
