8fa410d72b2f246c2c40c6794edd55616437161b
Kinsley Ajie Pereira <15153821@suicobrasileira.sp>
2016-10-26 15:29:01 -0200

    y

--- public/js/angular/watterizerApp.js
+++ public/js/angular/watterizerApp.js
@@ -4,7 +4,6 @@ app.run(function ($http) {
 	$http.get("/sessao")
 	.then(function (response){
 		$http.defaults.headers.common.token = response.data[1];
-		console.log($("#name"))
 		$("#name").html(response.data[0]);
 	});
 });
