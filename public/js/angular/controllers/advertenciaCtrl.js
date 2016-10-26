3f0fd718212aa8c35beb0204fafffdb235a790f7
Rodrigo <rodrigo14dasilva@hotmail.com>
2016-10-26 10:09:45 -0200

    Problema datatables resolvido

--- public/js/angular/controllers/advertenciaCtrl.js
+++ public/js/angular/controllers/advertenciaCtrl.js
@@ -1,14 +1,19 @@
 // CONTROLLER DE ADVERTENCIAS
+app.requires.push('datatables');
 app.controller('advertenciaCtrl', function($scope, $http) {
 	// LISTA TODAS AS ADVERTENCIAS
-	setTimeout(function (arguments) {
-		$http.get("/dados/advertencia")
+		$scope.showDT = true;
+		setTimeout(function() {
+			$http.get("/dados/advertencia")
 		.then(function (response) {
 			// console.log(.token);
 			$scope.advertencias = response.data;
-			console.log(response);
+			
 		}, function(response){
 			console.log("Falhou");
 		})
-	}, 10);
+		}, 0);
+		
+
+
 });
