d5b1be1ae3be9c2b87fc55d9b42fe241d06c0066
Kinsley Ajie Pereira <15153821@suicobrasileira.sp>
2016-11-01 16:52:37 -0200

    Funcionamento do Angular-NVD3 Chart

--- public/js/angular/controllers/gastoCtrl.js
+++ public/js/angular/controllers/gastoCtrl.js
@@ -2,36 +2,42 @@
 app.requires.push('nvd3');
 app.controller("gastoCtrl", function ($rootScope, $scope, $http, $interval, $timeout) {
 	$timeout(function() {
-		var socket = io.connect('localhost:1515');
 		$http.get("/dados/gasto/hoje")
 		.then(function (response) {
-			for (var j = 0; j <= response.data.length - 1; j++) {
-				$rootScope.gastos.push({x: j, y: Number(response.data[j].substr(response.data[j].lastIndexOf("\'")+1))});
+			$rootScope.gastos = [];
+			for (var i = 0; i <= response.data.length - 1; i++) {
+				$rootScope.gastos.push({x: i, y: Number(response.data[i].substr(response.data[i].lastIndexOf("\'")+1))});
 			}
-			$rootScope.gastos = response.data;
+			$rootScope.i = i;
+			$scope.getRecentData();
+		}, 100);
+
+		$scope.getRecentData = function () {
+			var socket = io.connect('localhost:1515');
 			socket.emit("load",2);
 			socket.on('toClientLoad', function (data) {
-				data=["1':'1","2':'2","3':'3","4':'4","5':'5","6':'2","7':'3","8':'4","9':'1","10':'2","11':'3","12':'4"];
 				if (data==null) {
 					console.log('null');
 					data=[];
 				}
-				for (var i = 0; i <= data.length - 1; i++) {
-		    		$scope.prepareChart.data.push(Number(data[i].substr(data[i].lastIndexOf("\'")+1)));
-		    		// if(i % 5 == 0){
-		      			$scope.prepareChart.labels.push(j);
-		      		// } else {
-		      			// $scope.prepareChart.labels.push(" ");
-		      		// }
-	    			j++;
+				for (j = 0; j <= data.length - 1; j++) {
+					$scope.gastos.push({x: $rootScope.i, y: Number(data[j].substr(data[j].lastIndexOf("\'")+1))});
+					$rootScope.i++;
 				}
-				$scope.prepareChart.data.splice(0, $scope.prepareChart.data.length - limit);
-				$scope.prepareChart.labels.splice(0, $scope.prepareChart.labels.length - limit);
+				$timeout(function(argument) {
+					$scope.createChart($scope.gastos);
+				},1000);
 			});
-		}, 100);
-		$scope.createChart = function () {
+			$interval(function(){
+				$scope.gastos.push({x: $rootScope.i, y: 200});
+				$scope.createChart($scope.gastos);
+				$rootScope.i++;
+			}, 1000);
+		}
+
+		$scope.createChart = function (data) {
 			$scope.chart = {};
-			$scope.options = {
+			$scope.chart.options = {
 				chart: {
 					type: 'lineChart',
 					height: 450,
@@ -90,41 +96,38 @@ app.controller("gastoCtrl", function ($rootScope, $scope, $http, $interval, $tim
 					}
 				}
 			};
-			$scope.data = sinAndCos();
-
+			$scope.chart.data = sinAndCos(data);
 			/*Random Data Generator */
-			function sinAndCos() {
-				var sin = [],sin2 = [],
-				cos = [];
-				var real = [];
-				var projection = [];
-				real.splice(0, real.length - 40);
-				projection.splice(0, projection.length - 40);
-	            //Data is represented as an array of {x,y} pairs.
-	            for (var i = 0; i < 41; i++) {
-	            	sin.push({x: i, y: Math.sin(i/10)});
-	            	sin2.push({x: i, y: i % 10 == 5 ? null : Math.sin(i/10) *0.25 + 0.5});
-	            	cos.push({x: i, y: .5 * Math.cos(i/10+ 2) + Math.random() / 10});
-	            }
+			function sinAndCos(data) {
+				// var sin = [],sin2 = [],
+				// cos = [];
+				var limit = 40;
+				data.splice(0, data.length - limit);
+	            // //Data is represented as an array of {x,y} pairs.
+	            // for (var i = 0; i < 41; i++) {
+	            // 	sin.push({x: i, y: Math.sin(i/10)});
+	            // 	sin2.push({x: i, y: i % 10 == 5 ? null : Math.sin(i/10) *0.25 + 0.5});
+	            // 	cos.push({x: i, y: .5 * Math.cos(i/10+ 2) + Math.random() / 10});
+	            // }
 
 	            //Line chart data should be sent as an array of series objects.
 	            return [
 	            	{
-	                    values: real,      //values - represents the array of {x,y} data points
+	                    values: data,      //values - represents the array of {x,y} data points
 	                    key: 'Hoje', //key  - the name of the series.
 	                    color: '#FFC814'  //color - optional: choose your own line color.
-	                },
-	                /*{
+	                }/*,
+	                {
 	                	values: cos,
 	                	key: 'Cosine Wave',
 	                	color: '#2ca02c'
-	                },*/
+	                },
 	                {
-	                	values: projection,
+	                	values: ,
 	                	key: 'Projeção',
 	                	color: '#7777ff',
 	                    area: false      //area - set to true if you want this line to turn into a filled area chart.
-	                }
+	                }*/
 	            ];
             };
         };
