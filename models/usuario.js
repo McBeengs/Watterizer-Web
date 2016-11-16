0645e60d8fef7e8f07bce83bea8d8b854ae62ba6
Rodrigo Marques da Silva <15153827@suicobrasileira.sp>
2016-11-16 16:50:59 -0200

    azdasd

--- models/usuario.js
+++ models/usuario.js
@@ -292,6 +292,9 @@ else{
 						
 					});
 				}
+				else{
+					res.end();
+				}
 			});
 			con.release();
 			token = null;
@@ -302,36 +305,34 @@ else{
 		connection.acquire(function(err, con) {
 			var sess = req.session;
 			var token = randtoken.generate(16);	
-			con.query('SELECT * FROM usuario WHERE (email = ? OR username = ?) AND senha = ? AND data_exclusao IS NULL', [login,login,senha], function(err, result) {
+			con.query('SELECT * FROM usuario INNER JOIN perfil ON(usuario.id_perfil=perfil.id) WHERE (email = ? OR username = ?) AND senha = ? AND data_exclusao IS NULL', [login,login,senha], function(err, result) {
 				if (result[0] != null) {
 					if (result[0].username!="admin" || result[0].token_web==null) {
 						con.query('UPDATE usuario SET token_web = ? WHERE (email = ? OR username = ?) AND senha = ?', [token, login,login,senha], function(err, result) {
 						});
 					}
-					con.query('SELECT usuario.id,username,senha,email,nome,telefone,hora_entrada,id_perfil,id_setor,id_pergunta,resposta_pergunta,token_web,token_desktop,hora_saida,hora_intervalo,data_exclusao,perfil.perfil FROM usuario INNER JOIN perfil ON(usuario.id_perfil=perfil.id) WHERE (email = ? OR username = ?) AND senha = ?', [login,login,senha], function(err, result) {
-						var string = JSON.stringify(result);
-						var obj = string;
-						if (obj!='[]' && result[0].perfil.toLowerCase()=='administrador') {
-							
-							sess.nome=result[0].nome;
-							sess.idUser=result[0].id;
-							sess.perfil=result[0].perfil;
-							sess.token=result[0].token_web;
-							
-							res.redirect("/portal");
-						}
-						else {
-							sess.login=false;
-							
-							res.redirect("/index");		
-						}					
-						
-					});
+					if (result[0].perfil.toLowerCase()=='administrador') {
+
+						sess.nome=result[0].nome;
+						sess.idUser=result[0].id;
+						sess.perfil=result[0].perfil;
+						sess.token=result[0].token_web;
+
+						res.redirect("/portal");
+					}
+					
+				}
+				else {
+					sess.login=false;
+
+					res.redirect("/index");	
+
+
 				}
 			});
-con.release();
-});
-};
+			con.release();
+		});
+	};
 } 
 
 module.exports = new Usuario();
