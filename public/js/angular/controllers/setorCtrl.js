de374be2540895747388cdf90f7dbb99520fa2b6
Kinsley Ajie Pereira <15153821@suicobrasileira.sp>
2016-11-23 14:52:28 -0200

    Canvas pt2

--- public/js/angular/controllers/setorCtrl.js
+++ public/js/angular/controllers/setorCtrl.js
@@ -12,9 +12,13 @@ app.controller('setorCtrl', function($scope, $http) {
 		});
 		$http.get("/dados/computadores")
 		.then(function (response) {
-			$scope.setores = response.data;
-			$scope.canvasSel = response.data[0];
-			console.log($scope.setores)
+			$scope.computadores = [];
+			for (var i = 0; i <= response.data.length - 1; i++) {
+				if (response.data[0].posicionado==0) {
+					$scope.computadores.push(response.data[i]);
+				}
+			}
+			console.log($scope.computadores)
 		}, function(response){
 			console.log("Falhou")
 		});
